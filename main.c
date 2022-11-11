75caf5f72364e1427c4e7b67885e7e0c423dcf3b
Johnnie Walker <jw12866@gmail.com>
2022-11-10 20:44:32 -0500

    inserts plus displays

--- main.c
+++ main.c
@@ -10,44 +10,82 @@ struct node {
   struct node *next;
 };
 
-struct node *next;
-struct node *prev;
+struct node *head = NULL;
+struct node *last = NULL;
 struct node *current = NULL;
 
-int insert_first(int key, int data);
+bool isEmpty() {
+  return head == NULL;
+}
+
+void insert_first(int key, int data);
+void insert_last(int key, int data);
+void display_forward();
+void display_backward();
 
 int main()
 {
-  struct node root;
-  struct node *proot = &root;
-  root.prev = NULL;
-  root.next = NULL; 
-  
-  add_node(proot, 10);
-  add_node(proot, 20);
-
-  printf("data: %d\n", proot->data);
-  
+  insert_last(3, 5);
+  insert_last(4, 7);
+  insert_first(1, 1);
+  display_forward();
+  display_backward();
   return 0;
 }
 
-int add_node(struct node *n, int data)
+void insert_first(int key, int data)
 {
-  struct node *tmp = (struct node*) malloc(sizeof(struct node));
-  int i = 0;
+  struct node *link = (struct node*) malloc(sizeof(struct node));
+  link->key = key;
+  link->data = data;
+  
+  if(isEmpty()) {
+    last = link;
+  } else {
+    head->prev = link;
+  }
+  link->next = head;
+  head = link;
+}
 
-  for (i = 0; n->next == NULL; ++i) {
-    printf("i %d\n", i);
-    tmp = n;
-    printf("set tmp\n");
-    n = tmp->next;
-    printf("set next");
-    n->prev = tmp;
+void insert_last(int key, int data)
+{
+  struct node *link = (struct node*) malloc(sizeof(struct node));
+  link->key = key;
+  link->data = data;
+
+  if(isEmpty()) {
+    head = link; 
+  } else {
+    last->next = link;
+  }
+  link->prev = last;
+  last = link;
+}
+
+void display_forward() {
+  struct node *ptr = head;
+
+  printf("[ ");
+	 
+  while (ptr != NULL) {
+    printf(" k: %d, d: %d ", ptr->key, ptr->data);
+    ptr = ptr->next;
   }
 
-  printf("now assign data\n");
-  n->data = data;
-  
-  printf("%4d loc added\n", i);
-  return i;
+  printf(" ]\n");
 }
+
+void display_backward() {
+  struct node *ptr = last;
+
+  printf("[ ");
+	 
+  while (ptr != NULL) {
+    printf(" k: %d, d: %d ", ptr->key, ptr->data);
+    ptr = ptr->prev;
+  }
+
+  printf(" ]\n");
+}
+  
